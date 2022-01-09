05845cd8f723ca2fd290777a45aec06e235cb71a
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-09 22:23:32 +0500

    object type

---
+++ typescript-types/object.ts
@@ -0,0 +1,14 @@
+let employee: object;
+
+employee = {
+    firstName: 'Jakhogir',
+    lastName: 'Bakhodirov',
+    age: 21,
+    jobTitle: 'Software Engineer'
+};
+
+console.log(employee);
+
+let vacant: {} = {};
+
+console.log(vacant.toString());
