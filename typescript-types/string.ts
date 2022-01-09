2c3f7ac3f8d4431a7a09ca3afaddf22546559a40
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-09 19:48:35 +0500

    string type

---
+++ typescript-types/string.ts
@@ -0,0 +1,12 @@
+let firstName: string = 'Jakhongir';
+let job: string = "Software Engineer";
+
+let description = `This TypeScript string can 
+span multiple 
+lines
+`;
+
+let profile: string = `I'm ${firstName}. 
+I'm a ${job}`;
+
+console.log(profile);
