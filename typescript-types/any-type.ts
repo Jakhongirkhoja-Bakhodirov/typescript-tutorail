38c205ec1f3ef2f8c511527a370c2dd5ef5966e0
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-09 23:06:11 +0500

    any type

---
+++ typescript-types/any-type.ts
@@ -0,0 +1,12 @@
+let anyValue;
+let result: any;
+result = 10.123;
+console.log(result.toFixed());
+// result.willExist(); //
+
+console.log(typeof(anyValue));
+anyValue = 'John'
+console.log(typeof(anyValue));
+anyValue = 20;
+console.log(typeof(anyValue));
+
