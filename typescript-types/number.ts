494ca9b7a15904d28b41f4712831e4a5e4ad0683
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-09 19:46:17 +0500

    Numbers type

---
+++ typescript-types/number.ts
@@ -0,0 +1,25 @@
+const birth_year:number = 2000;
+
+let salary:number = 400;
+
+//Binary Numbers
+let bin = 0b100;
+let anotherBin: number = 0B010;
+
+console.log(`Binary number\nbin => ${bin} \nanotherBin => ${anotherBin}`);
+
+//Octal number
+let octal: number = 0o10;
+
+console.log(`Octal => ${octal}`);
+
+
+//Hexadecimal numbers
+let hexadecimal: number = 0XA;
+
+console.log(`hexadecimal => ${hexadecimal}`);
+
+//Big Integers
+let bigint: bigint = 9007199254740991n;
+
+console.log(`bigint => ${bigint}`);
