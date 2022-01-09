28bccb94fff184ab1a00d780eb8e5e1e79419743
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-09 22:33:49 +0500

    array type

---
+++ typescript-types/array.ts
@@ -0,0 +1,12 @@
+let skills: string[] = ['problem solving' , 'programming'];
+
+skills.push('system design');
+
+let salary:number[] = [200,400];
+salary.push(2000);
+
+let scores:(string|number)[] = ['Problem sovling' , 4.5 , 'Programming' , 4.5 , 'System Design' , 4];
+console.log(`My Scores => ${scores}`);
+
+let myProfile:(string|number|boolean)[] = ['Problem sovling' , 4.5 , true, 'Programming' , 4.5 , true , 'System Design' , 4 , false];
+console.log(`My profile => ${myProfile}`);
