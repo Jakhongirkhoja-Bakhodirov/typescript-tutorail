fe035d7ad806a00623a7ef69485d340db4d1d6f4
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-10 08:53:07 +0500

    switch-case

--- control-flow-statement/index.ts
+++ control-flow-statement/index.ts
@@ -25,3 +25,56 @@ if (itemCount > 0 && itemCount <= 5) {
 
 console.log(`You got ${discount}% discount. `)
 
+//A simple switch-case example
+let targetId = 'btnDelete';
+
+switch (targetId) {
+    case 'btnUpdate':
+        console.log('Update');
+        break;
+    case 'btnDelete':
+        console.log('Delete');
+        break;
+    case 'btnNew':
+        console.log('New');
+        break;
+}
+
+//A group swtich-case example
+
+// change the month and year
+let month = 1,
+    year = 2022;
+
+let day = 10;
+switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+        day = 31;
+        break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+        day = 30;
+        break;
+    case 2:
+        // leap year
+        if (((year % 4 == 0) &&
+            !(year % 100 == 0))
+            || (year % 400 == 0))
+            day = 29;
+        else
+            day = 28;
+        break;
+    default:
+        throw Error('Invalid month');
+}
+
+console.log(`The month ${month} in ${year} has ${day} days`);
+
