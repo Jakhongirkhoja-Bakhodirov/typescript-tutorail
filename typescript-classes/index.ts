dacceb21d9cb6ee909ccfb6a652e8dfb5d186e2c
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-01-30 16:43:16 +0500

    getter and setter

--- typescript-classes/index.ts
+++ typescript-classes/index.ts
@@ -1,39 +1,77 @@
-class Person {
-    firstName:string;
-    lastName:string;
-    age:number;
+// class Person {
+//     firstName:string;
+//     lastName:string;
+//     age:number;
     
-    private salary:number;
-    protected job:string;
+//     private salary:number;
+//     protected job:string;
 
+// q
 
-    constructor(firstName:string,lastName:string,age:number,salary:number,job:string) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.age = age;
-        this.salary = salary;
-        this.job = job;
+//     constructor(firstName:string,lastName:string,age:number,salary:number,job:string) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.age = age;
+//         this.salary = salary;
+//         this.job = job;
+//     }
+
+//     setters(salary:number) {
+//         this.salary = salary;
+//     }
+
+//     getters() {
+//         return this.salary;
+//     }
+
+//     getFullName():string {
+//         return `${this.firstName} ${this.lastName}`;
+//     }
+// }
+
+// const somebody = new Person('Bob' , 'Mark' , 23 , 300 , 'software engineer');
+
+// console.log(somebody.getFullName());
+
+// console.log(somebody.getters());
+
+// somebody.setters(2000);
+
+// console.log(somebody.getters());
+class Person {
+    private _age:number;
+    private _firstName:string;
+    private _lastName:string;
+
+    constructor(firstName:string , lastName:string , age:number) {
+        this._age = age;
+        this._firstName = firstName;
+        this._lastName = lastName;
     }
 
-    setters(salary:number) {
-        this.salary = salary;
+    public get age() {
+        return this._age;
     }
 
-    getters() {
-        return this.salary;
+    public set age(theAge:number) {
+        if(theAge <= 0 || theAge >= 200) {
+            throw new Error('The age is invalid');
+        }
+        this._age = theAge;
     }
 
-    getFullName():string {
-        return `${this.firstName} ${this.lastName}`;
+    public getFullName():string {
+        return `${this._firstName} ${this._lastName}`;
     }
 }
 
-const somebody = new Person('Bob' , 'Mark' , 23 , 300 , 'software engineer');
+let person = new Person('Bob' , 'Rice' , 20);
 
-console.log(somebody.getFullName());
 
-console.log(somebody.getters());
+console.log(person.age);
 
-somebody.setters(2000);
+person.age = 21;
 
-console.log(somebody.getters());
+console.log(person.getFullName());
+
+console.log(person.age);
