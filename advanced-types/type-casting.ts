51037a0d5baae18309e5853fde4fc7aecfaf48da
Bakhodirov-Jakhongir <bahodirovjahongirxoja@gmail.com>
2022-02-12 14:04:49 +0500

    type-casting

---
+++ advanced-types/type-casting.ts
@@ -0,0 +1,19 @@
+let grade:any;
+
+grade = 5;
+
+console.log(typeof(grade));
+
+let gradeInString = <string>grade;
+
+
+let whichTypeIsIt:any = 43;
+
+console.log(typeof(whichTypeIsIt));
+
+
+whichTypeIsIt = 'is this answer to everything?';
+
+console.log(typeof(whichTypeIsIt));
+
+
